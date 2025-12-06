a63b1b0d0e169f509fcf7f3ea8f05bebc99ad754
Ivan Yelizariev <info@odoomagic.com>
2025-12-06 15:41:38 +0100

    fix-fix

--- src/index.js
+++ src/index.js
@@ -7,13 +7,9 @@ import { App } from './App'
 function Overlay() {
   return (
     <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
-      <a href="https://pmnd.rs/" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
-        pmnd.rs
-        <br />
-        dev collective
-      </a>
-      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>@mydream42.com</div>
-      <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>10/17/2021</div>
+      <div style={{ position: 'absolute', bottom: 40, left: 40, fontSize: '13px' }}>banana.mydream42.com</div>
+      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}><a href="https://mydream42.com">*.mydream42.com</a></div>
+      <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>https://tandemm.be/</div>
     </div>
   )
 }
